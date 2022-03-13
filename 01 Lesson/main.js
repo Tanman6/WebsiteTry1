dd0f3a260438074686e5ea697d6a780ae44024a4
Tanman6 <tanmansyx@protonmail.com>
2022-03-13 15:09:27 -0500

    update for menu and theme

--- 01 Lesson/main.js
+++ 01 Lesson/main.js
@@ -1,34 +1,41 @@
 // grab elements
-const selectElement = selector => {
+const selectElement = (selector) => {
     const element = document.querySelector(selector)
     if(element) return element;
-    throw new Error('Something went wrong, make sure that ${selector} exists or is typed correctly')
+    throw new Error('Something went wrong, make sure that ${selector} exists or is typed correctly');
 };
 
-//styles on scroll
-const scrolHeader = () => {
-    const headerElement = selectElement('#header');
+//nav styles on scroll
+const scrollHeader = () =>{
+    const navbarElement = selectElement('#header');
     if(this.scrollY >= 15){
-        headerElement.classlist.add('activated');
+        navbarElement.classlist.add('activated');
     }else{
-        headerElement.classlist.remove('activated');
+        navbarElement.classlist.remove('activated');
     }
 };
+
+window.addEventListener('scroll', scrollHeader);
 
 const menuToggleIcon = selectElement('#menu-toggle-icon');
 
 const toggleMenu = () => {
     const mobileMenu = selectElement('#menu');
-    mobileMenu.classlist.tooggle('activated')
-    menuToggleIcon.classlist.toggleMenu('activated');
+    mobileMenu.classlist.toggle('activated');
+    menuToggleIcon.classlist.toggle('activated');
 };
 
 menuToggleIcon.addEventListener('click', toggleMenu);
 
-//switch theme
-const bodyElement = document.body;
-const themeToggleBtn = selectElement('theme-toggle-btn')
+//switch theme/ add to local storage
+const body = document.body;
+const themeToggleBtn = selectElement('theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme')
 
 themeToggleBtn.addEventListener('click', () => {
     bodyElement.classlist.toggle('light-theme');
 });
+// Check to see if there is a theme preference in local Storage, if so add the light theme to the body
+if (currentTheme) {
+    body.classList.add('light-theme');
+}
